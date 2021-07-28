a64f2b9f4b6f53aacc3ce91af5ea607cd4bbaab0
guoquansen <gqs616667914@gmail.com>
2021-07-28 22:13:15 +0800

    登陆注册修改

--- vue-software-outsourcing9098/src/views/Register.vue
+++ vue-software-outsourcing9098/src/views/Register.vue
@@ -3,12 +3,8 @@
   <el-container>
     <el-header>Header</el-header>
     <el-main>
-      <el-button type="primary" plain>
-        <router-link to="/providerReg">服务商</router-link>
-      </el-button>
-      <el-button type="primary" plain>
-        <router-link to="/employerReg">雇主</router-link>
-      </el-button>
+      <el-button type="primary" plain @click="providerReg">服务商</el-button>
+      <el-button type="primary" plain @click="employerReg">雇主</el-button>
     </el-main>
     <el-footer>Footer</el-footer>
   </el-container>
@@ -17,7 +13,15 @@
 
 <script>
 export default {
-  name: "Register"
+  name: "Register",
+  methods:{
+    providerReg(){
+      this.$router.push('/providerReg')
+    },
+    employerReg(){
+      this.$router.push('/employerReg')
+    }
+  }
 }
 </script>
 
