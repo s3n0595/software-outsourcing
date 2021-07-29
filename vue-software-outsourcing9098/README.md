943f5e630125915acd16f9ac7ef7455e11ffe3f4
guoquansen <gqs616667914@gmail.com>
2021-07-30 04:05:03 +0800

    Merge remote-tracking branch 'origin/quansen'
    # Conflicts:
    #	business-management9093/src/main/java/com/cykj/service/impl/ProviderServiceImpl.java
    #	business-management9093/target/classes/com/cykj/controller/RegisterController.class
    #	business-management9093/target/classes/com/cykj/mapper/EmployerMapper.class
    #	business-management9093/target/classes/com/cykj/mapper/ProviderMapper.class
    #	business-management9093/target/classes/com/cykj/service/EmployerService.class
    #	business-management9093/target/classes/com/cykj/service/ProviderService.class
    #	business-management9093/target/classes/com/cykj/service/impl/EmployerServiceImpl.class
    #	business-management9093/target/classes/com/cykj/service/impl/ProviderServiceImpl.class
    #	business-management9093/target/classes/mybatis/mapper/EmployerMapper.xml
    #	business-management9093/target/classes/mybatis/mapper/ProviderMapper.xml

--- vue-software-outsourcing9098/README.md
+++ vue-software-outsourcing9098/README.md
@@ -1,21 +1,39 @@
-# vue_talent_ecosystem
+# Shanghai kejian engineering management
+> 科健工程管理有限公司企业官网 前端    
 
-> A Vue.js project
+一个业余时间（三周）的外包项目 最终成品：http://www.shkjem.com    
+主分支已更新至改进版，老版本源码移至 Release  
 
-## Build Setup
+如果是学习的话推荐学习新版本 链接 http://kejian.filog.cn/    
+另外有些同学在问后端的代码 这里 https://github.com/ifzc/KeJian.Core.Api
 
-``` bash
-# install dependencies
+## 新版本改进
+1. 整体重构，结构升级为最新 vue-cli
+2. 代码优化，去除引用的静态js文件
+3. 图片加载加入懒加载，整体性能提升很大
+4. 视频采用 vue-video-player 处理兼容性
+5. 首页全屏滚动样式由静态文件 fullpage.js 改为 vue-awesome-swiper
+6. 样式调整，提升了自适应能力
+
+## 组件列表
+1. vue-router
+2. element-ui
+3. axios
+4. vue-video-player (视频组件)
+5. vue-awesome-swiper (首页滚动组件)
+6. vue-lazyload (图片懒加载)
+
+## Project setup
+```
 npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
 ```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
--- vue-software-outsourcing9098/src/main.js
+++ vue-software-outsourcing9098/src/main.js
@@ -1,32 +1,46 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import App from './App.vue'
 import router from './router'
-import Axios from 'axios'
-import qs from 'qs'
+import axios from 'axios'
 
-Vue.prototype.$axios = Axios;
-
-Axios.defaults.baseURL = 'api';
-
-// 导入 ElementUI
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-import App from './App'
-// import Axios from "axios";
-
-// 安装路由
-Vue.use(VueRouter);
-
-// 安装 ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
-Vue.prototype.$qs = qs;
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper);
+
+import VideoPlayer from 'vue-video-player'
+import 'video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+Vue.use(VideoPlayer)
+
+import VueLazyload from 'vue-lazyload'
+Vue.use(VueLazyload, {
+  //完全显示的时候加载
+  preLoad: 1,
+  //失败时显示的图片
+  error: require('./assets/img/error.png'),
+  //加载时显示的GIF图
+  loading: require('./assets/img/loading.gif'),
+  //尝试加载几次
+  attempt: 1
+});
+
+Vue.config.productionTip = false
+
+//设置超时时间
+axios.defaults.timeout = 5000
+//设置api地址
+//全局定义axios
+axios.defaults.baseURL = 'http://shkjgw.shkjem.com/api/'
+Vue.prototype.$http = axios
+
+// 图片服务器地址
+Vue.prototype.imgserver = 'http://shkjgw.shkjem.com/'
 
 new Vue({
-  el: '#app',
-  // 启用路由
   router,
-  // 启用 ElementUI
   render: h => h(App)
-});
+}).$mount('#app')
