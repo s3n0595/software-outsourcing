7df890851326d99f364ae92ca1376f8e18315c58
guoquansen <gqs616667914@gmail.com>
2021-07-30 14:28:57 +0800

    恢复

---
+++ vue-software-outsourcing9098/src/views/LoginChoose.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <banner title="登陆选择" />
+    <el-header>Header</el-header>
+    <el-main>
+      <el-button type="primary" plain @click="providerLog">服务商登陆</el-button>
+      <el-button type="primary" plain @click="employerLog">雇主登陆</el-button>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import Banner from "../components/Banner";
+export default {
+  name: "LoginChoose",
+  components: {
+    Banner
+  },
+  methods: {
+    providerLog(){
+      this.$router.push('/providerLog')
+    },
+    employerLog(){
+      this.$router.push('/employerLog')
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
