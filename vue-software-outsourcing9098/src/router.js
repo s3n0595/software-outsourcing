39caa1a7c74698b6ebb56242a9fb9f78adb3b11c
DDDJing <782294155@qq.com>
2021-07-30 15:07:03 +0800

    JD---支付宝例子9093

--- vue-software-outsourcing9098/src/router.js
+++ vue-software-outsourcing9098/src/router.js
@@ -10,18 +10,6 @@ let kejianrouter = new Router({
       component: () => import('./views/Home.vue')
     },
     {
-      //登陆选择
-      path: '/loginChoose',
-      name: 'loginChoose',
-      component: () => import('./views/LoginChoose')
-    },
-    {
-      //雇主登陆
-      path: '/employerLog',
-      name: "EmployerLog",
-      component: () => import('./views/EmployerLog')
-    },
-    {
       path: '/news',
       name: 'news',
       component: () => import('./views/News.vue'),
@@ -119,24 +107,24 @@ let kejianrouter = new Router({
 })
 
 // 判断是否需要登录权限 以及是否登录
-kejianrouter.beforeEach((to, from, next) => {
-  // 判断是否需要登录权限
-  if (to.matched.some(res => res.meta.requireAuth)) {
-    // 判断是否登录
-    if (sessionStorage.getItem('token')) {
-      next()
-    } else {
-      // 没登录则跳转到登录界面
-      next({
-        path: '/login',
-        query: {
-          redirect: to.fullPath
-        }
-      })
-    }
-  } else {
-    next()
-  }
-})
+// kejianrouter.beforeEach((to, from, next) => {
+//   // 判断是否需要登录权限
+//   if (to.matched.some(res => res.meta.requireAuth)) {
+//     // 判断是否登录
+//     if (sessionStorage.getItem('token')) {
+//       next()
+//     } else {
+//       // 没登录则跳转到登录界面
+//       next({
+//         path: '/login',
+//         query: {
+//           redirect: to.fullPath
+//         }
+//       })
+//     }
+//   } else {
+//     next()
+//   }
+// })
 
 export default kejianrouter
